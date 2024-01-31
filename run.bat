a998b6d4d50321bfa9df68614044dae4d81055c2
Anmol Budhiraja <anmolbudhiraja@Anmols-Air.lan1>
2024-01-30 19:28:31 -0800

    Update Starter Code

--- run.bat
+++ run.bat
@@ -2,9 +2,10 @@
 
 if "%1"=="" goto help
 
-javac -cp tester.jar %1.java
-java -classpath "tester.jar;%cd%" tester.Main %1
+del *.class
+javac -cp lib/tester.jar *.java
+java -Djava.security.manager=disallow -classpath lib/tester.jar;"%cd%" tester.Main %1
 goto :eof
 
 :help
-echo "Please provide the class name. For instance: .\run.bat ExampleTweets"
+echo "Please provide the class name. For instance: .\run.bat DesignRecipeExamples"
